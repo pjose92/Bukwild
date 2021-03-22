5251d8b13269076c19d2666102589388dc08eb70
jose perez <joseperez@joses-MacBook-Pro.local>
2021-03-21 22:29:47 -0500

    starting styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Buk from './Buk';
-import logo from './abc_logo.svg';
+import logo from './assets/abc_logo.svg';
 import dataContent from './data/db.json';
 
+// this will map through the db.json file to get new background image
 var images = dataContent.pages.map(value => require(`./backgrounds/${value.blocks[0].background}`));
 class App extends React.Component{
     constructor(){
@@ -14,8 +15,23 @@ class App extends React.Component{
  
     render(){
         return(
-
+            //assigning the bg image
+            <div style={{
+                backgroundImage: `url(${images[this.state.index].default})`
+                }} className="main-container">
+                <div className="top-ui">
+                    <div className="logo-contact-box">
+                        <img id="logo" src={logo} alt="website-logo"/>
+                        <div id="contact-button">
+                          Contact Us
+                        </div>
+                    </div>
+                    <div onClick={() => this.setState({index: 0})} className="nav-link">Industries</div>
+                    <div onClick={() => this.setState({index: 1})} className="nav-link">Services</div>
+                    <div onClick={() => this.setState({index: 2})} className="nav-link">About Us</div>
+                </div>
                 <Buk index={this.state.index} />
+            </div>
         );
     }
 }
--- src/Buk.js
+++ src/Buk.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import dataContent from './data/db.json';
-import arrow from './right-arrow.svg';
+import arrow from './assets/right-arrow.svg';
 
 class Buk extends React.Component{
     render(){
      return(
             <>  
                 <div id="test" className="headings">
+                    {/* Headline on the page  */}
+                    <div className="headline">
+                        {dataContent.pages[this.props.index].blocks[0].headline}
+                    </div>
+                    {/* this is the subhead line on the page  */}
+                    <div className="subhead">
+                        {dataContent.pages[this.props.index].blocks[0].subhead}
+                    </div>
                 </div>
-
                 <div className="cta-box fade-up">
-                    <div className="cta-text">{dataContent.pages[this.props.index].blocks[0].cta}</div>
+                    {/* text on the white bottom box */}
+                    <div className="cta-text">
+                        {dataContent.pages[this.props.index].blocks[0].cta}
+                    </div>
                     <div className="lets-talk-button">
-                        <span>LET'S TALK.</span>
+                        <span className="LETS-TALK">LET'S TALK.</span>
                         <img src={arrow} alt="arrow"/>
                     </div>
                 </div> 
